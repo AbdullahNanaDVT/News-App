9db7c7b6f00ee53da882a72fc185285608dff476
AbdullahNanaDVT <79845386+AbdullahNanaDVT@users.noreply.github.com>
2021-07-03 17:36:22 +0200

    Changed UI and added images

---
+++ News App/API/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  News App
+//
+//  Created by Abdullah Nana on 2021/07/01.
+//
+
+import Foundation
+
+struct APIKey {
+    static let key = "c48088d2c6704e9692a00e6de346f105"
+}
